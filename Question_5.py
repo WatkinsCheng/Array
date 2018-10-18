a2a63b1d352aa405dce3f4de17a8240993bb11ef
qiwenzheng <875826697@qq.com>
2018-10-18 18:44:40 +0800

    如何在未排序的整数数组中找到最大值与最小值

--- Question_5.py
+++ Question_5.py
@@ -1,4 +1,7 @@
 # If there are multiple duplicates in the array, how do you find duplicate Numbers
 arr = [1, 4, 4, 2, 2, 5, 6, 7, 8, 1]
 arrset = set(arr)
-print(arrset)
+for item in arrset:
+    num = arr.count(item)
+    if num > 1:
+        print(item)
